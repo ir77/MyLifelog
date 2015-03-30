36bdb66ca8e679b01abbd2ef0ffdb58eeebb0031
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-03-30 11:45:47 +0900

    輝度の取得

--- MyLifelog/ViewController.swift
+++ MyLifelog/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var brightness = UIScreen.mainScreen().brightness
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "screenBrightnessDidChange:", name: UIScreenBrightnessDidChangeNotification, object: nil)
+        self.myLabel.text = brightness.description
     }
+
+    func screenBrightnessDidChange(notification:NSNotification){
+        var screen : UIScreen = notification.object as UIScreen
+        self.myLabel.text = screen.brightness.description
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
