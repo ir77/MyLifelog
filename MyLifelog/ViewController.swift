f32c6adaa004817473d56d8ffc5b045d7344104c
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-01 20:40:52 +0900

    加速度と現在地を常時取得する

--- MyLifelog/ViewController.swift
+++ MyLifelog/ViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import CoreMotion
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var myLabel: UILabel!
+    var myMotionManager: CMMotionManager!
+    var locManager: CLLocationManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +22,35 @@ class ViewController: UIViewController {
         var brightness = UIScreen.mainScreen().brightness
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "screenBrightnessDidChange:", name: UIScreenBrightnessDidChangeNotification, object: nil)
         self.myLabel.text = brightness.description
+        self.setupLocationManager()
     }
-
+    
+    func setupLocationManager () {
+        self.locManager = CLLocationManager();
+        self.locManager!.delegate = self;
+        if (!CLLocationManager.locationServicesEnabled()) {
+            println("Location services are not enabled");
+        }
+        self.locManager!.requestAlwaysAuthorization();
+        self.locManager!.pausesLocationUpdatesAutomatically = false;
+        self.locManager!.startUpdatingLocation()
+        
+        // MotionManagerを生成.
+        myMotionManager = CMMotionManager()
+        
+        // 更新周期を設定.
+        myMotionManager.accelerometerUpdateInterval = 0.5
+        
+        // 加速度の取得を開始.
+        myMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(accelerometerData:CMAccelerometerData!, error:NSError!) -> Void in
+            println(accelerometerData.acceleration.x)
+        })
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        println(newLocation.timestamp)
+    }
+    
     func screenBrightnessDidChange(notification:NSNotification){
         var screen : UIScreen = notification.object as UIScreen
         self.myLabel.text = screen.brightness.description
