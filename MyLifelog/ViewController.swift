59c0daeef9d6a6a0ce72e8524882737379daf066
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-02 12:29:33 +0900

    update brightness data to parse by location update

--- MyLifelog/ViewController.swift
+++ MyLifelog/ViewController.swift
@@ -11,38 +11,75 @@ import CoreMotion
 import CoreLocation
 import Parse
 
+struct ParseSetting {
+    static var applicationId : String!
+    static var clientKey : String!
+    static var preBrightness : CGFloat!
+}
+
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var myLabel: UILabel!
     var locManager: CLLocationManager?
-    
+
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var brightness = UIScreen.mainScreen().brightness
+        ParseSetting.preBrightness = brightness
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "screenBrightnessDidChange:", name: UIScreenBrightnessDidChangeNotification, object: nil)
         self.myLabel.text = brightness.description
+        setupParse()
         self.setupLocationManager()
-        self.setupParse()
     }
-    
-    func setupParse () {
+    func screenBrightnessDidChange(notification:NSNotification){
+        var screen : UIScreen = notification.object as UIScreen
+        self.myLabel.text = screen.brightness.description
+    }
+
+    // MARK: - Parse
+    func setupParse () -> Bool {
         var myDict: NSDictionary?
         if let path = NSBundle.mainBundle().pathForResource("private", ofType: "plist") {
             myDict = NSDictionary(contentsOfFile: path)
         }
         if let dict = myDict {
-            let applicationId = dict["applicationId"] as String
-            let clientKey = dict["clientKey"] as String
+            ParseSetting.applicationId = dict["applicationId"] as String
+            ParseSetting.clientKey = dict["clientKey"] as String
             Parse.enableLocalDatastore()
-            Parse.setApplicationId(applicationId, clientKey: clientKey)
-            
-            let testObject = PFObject(className: "TestObject")
-            testObject["foo"] = "bar"
-            testObject.save()
+            Parse.setApplicationId(ParseSetting.applicationId, clientKey: ParseSetting.clientKey)
+            return true
+        }
+        return false
+    }
+    func saveBrightnessDataInParse () {
+        if !checkBrightnessChange() {
+            return
+        }
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = UIScreen.mainScreen().brightness.description
+        testObject.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError!) -> Void in
+            if (success) {
+                // The object has been saved.
+                println("success")
+            } else {
+                // There was a problem, check error.description
+                println("error")
+            }
         }
     }
+    func checkBrightnessChange () -> Bool {
+        let brightnessDiff =  abs(ParseSetting.preBrightness - UIScreen.mainScreen().brightness)
+        if (brightnessDiff >= 0.2) {
+            ParseSetting.preBrightness = UIScreen.mainScreen().brightness
+            return true
+        }
+        return false
+    }
     
+    // MARK: - Location
     func setupLocationManager () {
         self.locManager = CLLocationManager();
         self.locManager!.delegate = self;
@@ -53,7 +90,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.locManager!.pausesLocationUpdatesAutomatically = false;
         self.locManager!.startUpdatingLocation()
     }
-    
     func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
         println(newLocation.timestamp)
         var brightness = UIScreen.mainScreen().brightness
